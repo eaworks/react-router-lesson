474fdb2f5196d1f93d3a7c4d3a36d709e129b559
Ersin Aydın <ersinaydinworks@gmail.com>
2023-04-02 02:36:40 +0300

    add irl aboutus

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import AboutUs from './components/AboutUs';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
       </Routes>
     </div>
   );
