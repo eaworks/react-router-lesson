8b869a26637fca3f7293c166805608874a1f79e0
Ersin Aydın <ersinaydinworks@gmail.com>
2023-04-02 21:10:31 +0300

    add navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutUs" element={<AboutUs />} />
