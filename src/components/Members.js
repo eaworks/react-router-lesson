a56aed0bdf120d31a948693b26a65a9c7d8b8159
Ersin Aydın <ersinaydinworks@gmail.com>
2023-04-02 21:55:07 +0300

    useSearchParams filtre sistemi

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 function Members() {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const isActive = searchParams.get('filter') === 'active';
     return (
-        <div>Members</div>
+        <>
+            <div>Members</div>
+            <button onClick={() => setSearchParams({ filter: "active" })}>Aktif Uyeler</button>
+            <button onClick={() => setSearchParams()}>Tum Uyeler</button>
+            {isActive ? <h2>Aktif Uyeler</h2> : <h2>Tum Uyeler</h2>}
+        </>
     )
 }
 
